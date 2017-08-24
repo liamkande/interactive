ecce1c924914f49474214554b08e35d40701bc02
liamkande <liamkande@Ansoumanes-MBP.home>
2017-08-24 09:28:53 -0400

    development and production environment

---
+++ README.md
@@ -0,0 +1,3 @@
+# Interactive Resume
+## Project description
+Using jQuery, I developed an interactive resume application that reads all data from a JSON file and then dynamically modifies the DOM to display the information.
---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    browserify = require('gulp-browserify'),
+    webserver = require('gulp-webserver'),
+    uglify = require('gulp-uglify'),
+    jsmin = require("gulp-jsmin"),
+    concat = require('gulp-concat'),
+    jsonminify = require("gulp-jsonminify"),
+    minifyCSS = require("gulp-minify-css"),
+    minifyHTML = require("gulp-minify-html"),
+    gulpif = require('gulp-if');
+
+var env,
+    app,
+    jsSource,
+    //src = './components';
+    jsSource = [
+      //'components/js/ProjList.js',
+      //'components/js/app.js'
+    ];
+
+env = process.env.NODE_ENV || "development";
+//NODE_ENV=production gulp
+
+if (env=== "development") {
+  app = "builds/development/";
+  //sassStyle = "expanded";
+} else {
+  app = "builds/production/"; // this var replaces all builds/production
+  //sassStyle = "compressed";
+}
+
+/*
+gulp.task('js', function() {
+  return gulp.src("components/js/*.js")
+    .pipe(concat('script.js'))
+    .pipe(browserify({
+      transform: 'reactify',
+      debug: true
+    }))
+    .on('error', function (err) {
+      console.error('Error!', err.message);
+    })
+    .pipe(gulpif(env === "production", jsmin()))
+    .pipe(gulp.dest(app + 'js'))
+});
+*/
+gulp.task('fx', function() {
+  gulp.src("builds/development/js/*.js")
+  .pipe(gulpif(env === "production", jsmin()))
+  .pipe(gulpif(env === "production", gulp.dest("builds/production/js")))
+});
+
+gulp.task('html', function() {
+  gulp.src("builds/development/*.html")
+  .pipe(gulpif(env === "production", minifyHTML()))
+  .pipe(gulpif(env === "production", gulp.dest(app)))
+});
+
+gulp.task('css', function() {
+  gulp.src("builds/development/css/*.css")
+    .pipe(gulpif(env === "production", minifyCSS()))
+    .pipe(gulpif(env === "production", gulp.dest("builds/production/css")))
+});
+/*
+gulp.task('json', function() {
+  gulp.src("builds/development/js/*.json")
+    .pipe(gulpif(env === "production", jsonminify()))
+    .pipe(gulpif(env === "production", gulp.dest("builds/production/js")))
+});
+*/
+
+gulp.task('watch', function() {
+  gulp.watch( app + '/js/**/*', ['fx']);
+  gulp.watch( app + '/css/**/*.css', ['css']);
+  gulp.watch([ app + '/**/*.html'], ['html']);
+});
+
+gulp.task('webserver', function() {
+  gulp.src( app + '/')
+    .pipe(webserver({
+        livereload: true,
+        open: true
+    }));
+});
+
+gulp.task('default', ['watch', 'html','css','fx', 'webserver']);
